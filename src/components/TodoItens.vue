aa0c82b4a025ab68cb2c80f4e2e9e7347ebb64b4
risu <cleber.risu@outlook.com>
2024-07-26 19:34:24 -0300

    composing the content

---
+++ src/components/TodoItens.vue
@@ -0,0 +1,17 @@
+<script>
+import TodoItem from "./TodoItem.vue";
+
+export default {
+  components: {
+    TodoItem,
+  },
+};
+</script>
+
+<template>
+  <div class="space-y-2">
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+  </div>
+</template>
